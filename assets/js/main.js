ecd9aea0922caf5e7e2ed7739777073c65ec1fad
Jassem Manita <jasemmanita00@gmail.com>
2023-11-17 17:29:06 +0100

    Delete assets directory

--- assets/js/main.js
+++
@@ -1,7 +0,0 @@
-function openSlideMenu() {
-    document.getElementById("side-menu").style.width = "250px"; 
-}
-
-function closeSlideMenu() {
-    document.getElementById("side-menu").style.width = "0"; 
-}
